ca6d09f4b9f3c7b7b7124def9051cf7b21be0805
Dustin Bracy <dustin.bracy@flmarine.com>
2021-07-07 15:25:40 -0500

    add resistor-color

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,10 +1,15 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const colorValues = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+}
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
-};
-
-export const COLORS = undefined;
+export const colorCode = (color) => colorValues[color];
+export const COLORS = Object.keys(colorValues);
