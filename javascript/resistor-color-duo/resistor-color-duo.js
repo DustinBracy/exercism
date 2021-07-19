35602625358c7a2a38d440ac6a6d2b03d7411fcc
Dustin Bracy <dustin.bracy@flmarine.com>
2021-07-19 10:15:20 -0500

    refactor to remove unneeded const

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,6 +1,6 @@
 import { colorCode } from "../resistor-color/resistor-color";
 
-export const decodedValue = (colors) => {
-  const [band1, band2] = colors
+export const decodedValue = ([band1, band2]) => {
   return colorCode(band1) * 10 + colorCode(band2)
 };
+
