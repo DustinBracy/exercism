1a29c2ddcb2667609521b4fe9ec44937a23ea6b4
Dustin Bracy <dustin.bracy@flmarine.com>
2021-07-19 10:15:31 -0500

    add acronym py

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,7 @@
+import re
+
+
 def abbreviate(words):
-    pass
+    words = re.sub(r"-", " ", words)
+    words = re.sub(r"[^A-Z ]", "", words.upper())
+    return "".join(word[0] for word in words.split())
