d24b570cab15074efd3a1f89c82ea87e56fd9c75
Dustin Bracy <bracy.dustin@gmail.com>
2022-09-19 11:06:06 -0500

    add submissions

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather provides a weather forecast for a given city and condition.
 package weather
 
+//CurrentCondition provides the current weather condition.
 var CurrentCondition string
+//CurrentLocation identifies the city in which the weather applies.
 var CurrentLocation string
 
+//Forecast gives the current weather condition for a city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
