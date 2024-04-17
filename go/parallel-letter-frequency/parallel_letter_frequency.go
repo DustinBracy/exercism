db8ea35ce21ff6f471d5fbeea28e6eb12fb52b40
Dustin Bracy <dustin.bracy@flmarine.com>
2024-04-17 15:12:03 -0500

    submission

---
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -0,0 +1,45 @@
+package letter
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(text string) FreqMap {
+	frequencies := FreqMap{}
+	for _, r := range text {
+		frequencies[r]++
+	}
+	return frequencies
+}
+
+// ConcurrentFrequency counts the frequency of each rune in the given strings,
+// by making use of concurrency.
+
+func ConcurrentFrequency(texts []string) FreqMap {
+	frequencies := FreqMap{}
+	ch := make(chan FreqMap, len(texts))
+	for _, text := range texts {
+		go func(text string) {
+			ch <- Frequency(text)
+		}(text)
+	}
+	for i := 0; i < len(texts); i++ {
+		freq := <-ch
+		for k, v := range freq {
+			frequencies[k] += v
+		}
+	}
+	return frequencies
+}
+
+func SequentialFrequency(texts []string) FreqMap {
+	frequencies := FreqMap{}
+	for _, text := range texts {
+		freq := Frequency(text)
+		for k, v := range freq {
+			frequencies[k] += v
+		}
+	}
+	return frequencies
+}
